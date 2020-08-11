a64d41322b12a63dd9922a16d15ef215d819b746
ClaireSheek <clairesheek@gmail.com>
2020-08-10 19:55:58 -0500

    added js to resume

---
+++ scripts/resume.js
@@ -0,0 +1,6 @@
+// user click: add fade to name
+
+function userClick(element) {
+    console.log("this should be my name", element);
+    element.className += "animate__animated animate__fadeIn"
+}
