68068d6f3eba59f83d4b99fac4bc885626b48abc
ClaireSheek <clairesheek@gmail.com>
2020-08-10 20:47:43 -0500

    stuck on the final annimation requirement

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,6 +1,25 @@
-// user click: add fade to name
-
-function userClick(element) {
+function fadeOnClick(element) {
     console.log("this should be my name", element);
     element.className += "animate__animated animate__fadeIn"
 }
+
+function hideOnHover(element) {
+    console.log("this should be photo", element)
+    element.className += "animate__animated animate__fadeOut"
+}
+
+function revealPostHover(element) {
+    element.className += "animate__animated animate__fadeIn"
+}
+
+function revealNum() {
+    const x = document.getElementById('phoneNum');
+    if (x.style.visibility === 'visable') {
+        x.style.visibility = 'hidden' ;
+    } else {
+        x.styles.visibility = 'visable';
+    }
+}
+
+    // document.getElementById('phoneNum') += "animate__animated animate__fadeOut"
+    // document.getElementById(phoneNum).style.display = '';
