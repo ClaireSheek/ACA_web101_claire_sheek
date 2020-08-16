8b961a4488592144aa51a02cea187783be4f0ebf
ClaireSheek <clairesheek@gmail.com>
2020-08-16 16:17:31 -0500

    assignment complete

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,3 +1,5 @@
+// user click: add fade to name
+
 function fadeOnClick(element) {
     console.log("this should be my name", element);
     element.className += "animate__animated animate__fadeIn"
@@ -8,18 +10,15 @@ function hideOnHover(element) {
     element.className += "animate__animated animate__fadeOut"
 }
 
+
 function revealPostHover(element) {
     element.className += "animate__animated animate__fadeIn"
 }
 
-function revealNum() {
-    const x = document.getElementById('phoneNum');
-    if (x.style.visibility === 'visable') {
-        x.style.visibility = 'hidden' ;
-    } else {
-        x.styles.visibility = 'visable';
-    }
+function revealNum() { 
+    const x = document.getElementById("phoneNum"); 
+    if (x.style.display === "none") { 
+        x.style.display = "block"; } 
+    else { x.style.display = "none"; 
+    } 
 }
-
-    // document.getElementById('phoneNum') += "animate__animated animate__fadeOut"
-    // document.getElementById(phoneNum).style.display = '';
